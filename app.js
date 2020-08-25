e69c9613747f292c245011b673e5cbc73c28e173
Oleksandr Volkov <fromobileprima@gmail.com>
2020-08-25 21:01:15 +0300

    connect data base

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const config = require('config');
+const mongoose = require("mongoose");
+
+const app = express();
+
+const PORT = config.get('port') || 5000
+
+async function start() {
+    try {
+        await mongoose.connect(config.get('mongoUri'), {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useUnifiedTopology: true,
+        });
+
+        app.listen(PORT, () => console.log(`Listren server on ${PORT}`))
+    } catch (err) {
+        console.log("Server Error", err.message);
+        process.exit()
+    }
+}
+
+start();
