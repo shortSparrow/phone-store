ef259f626729707a2dfac1cf5c5203f701390d93
Senya <senya@AnyMacs-MacBook-Pro.local>
2020-09-16 21:17:57 +0300

    finish paginaation

--- client/src/components/PhoneCardList/PhoneCardList.tsx
+++ client/src/components/PhoneCardList/PhoneCardList.tsx
@@ -11,17 +11,23 @@ interface cardListInterface {
 }
 
 const PhoneCardList: React.FC<cardListInterface> = ({ phoneList }) => {
-    // console.log(phoneList);
     const [list, setList] = useState([]);
     const [structureList, setStructureList] = useState<any>({
         onPage: 16,
         currentPage: 1,
         pages: [],
         data: [],
-        currentVissible: []
+        currentVissible: [],
+        loaded: false
     })
 
-    // const currentPageRef = useRef<HTMLInputElement>(null)
+    const [paginationList, setPaginationList] = useState<any>([]);
+
+    useEffect(() => {
+        if (structureList.loaded) {
+            updatePagination()
+        }
+    }, [structureList])
 
     const test = (arr: any) => {
         setList(arr)
@@ -30,11 +36,9 @@ const PhoneCardList: React.FC<cardListInterface> = ({ phoneList }) => {
             currentPage: 1,
             pages: Array.from(Array(Math.ceil(arr.length / 5)).keys(), (_, i) => i + 1),
             data: arr,
-            currentVissible: arr.slice(0, 5)
+            currentVissible: arr.slice(0, 5),
+            loaded: true
         })
-
-        console.log(new Array(Math.ceil(arr.length / 5)));
-
     }
 
     useEffect(() => {
@@ -43,10 +47,7 @@ const PhoneCardList: React.FC<cardListInterface> = ({ phoneList }) => {
             arrEmpty.length = 100;
             arrEmpty.fill(phoneList[0]);
             test(arrEmpty)
-            // console.log(phoneList);
         }
-
-
     }, [phoneList])
 
     const handlePageStructure = (event: any) => {
@@ -59,41 +60,54 @@ const PhoneCardList: React.FC<cardListInterface> = ({ phoneList }) => {
             currentPage: 1,
         })
     }
-    useEffect(() => {
-        const visibleList = () => {
-            let leftStep = 2;
-            let rightStep = 2;
 
-            while (structureList.currentPage - leftStep <= 0) {
-                leftStep--;
-                rightStep++;
-            }
+    const updatePagination = (page: any = null) => {
+        const { data, onPage } = structureList;
 
-            while (structureList.currentPage + rightStep > structureList.pages.length) {
-                leftStep++;
-                rightStep--
-            }
+        let leftStep = 3;
+        let rightStep = 3;
 
-            let space = [structureList.currentPage - leftStep, structureList.currentPage, structureList.currentPage + rightStep];
-
-            let i = 0;
-            let luft = [];
-
-            while ((structureList.currentPage + rightStep - i) - (structureList.currentPage - leftStep)) {
-                luft.push(structureList.currentPage - leftStep + i)
-                i++;
-            }
-
-            luft[0] = 1;
-
-            luft.push(structureList.pages.length)
-
-
-            return luft;
+        while (structureList.currentPage - leftStep <= 0) {
+            leftStep--;
+            rightStep++;
         }
 
-        console.log(visibleList());
-    }, [structureList])
+        while (structureList.currentPage + rightStep > structureList.pages.length) {
+            leftStep++;
+            rightStep--
+        }
+
+        let space = [structureList.currentPage - leftStep, structureList.currentPage, structureList.currentPage + rightStep];
+
+        let i = 0;
+        let luft = [];
+
+        while ((structureList.currentPage + rightStep - i) - (structureList.currentPage - leftStep)) {
+            const newValue = structureList.currentPage - leftStep + i;
+            if (newValue > 0) {
+                luft.push(newValue)
+            }
+            i++;
+        }
+
+        luft[0] = 1;
+
+        luft.push(structureList.pages.length)
+
+        setPaginationList(luft);
+        if (page) {
+            setStructureList({
+                ...structureList,
+                currentPage: page,
+                currentVissible: data.slice((page! - 1) * onPage, onPage * page! + 1)
+            })
+        }
+        // return luft;
+    }
+
+    useEffect(() => {
+        console.log(paginationList);
+    }, [paginationList])
 
 
     return (
@@ -109,75 +123,111 @@ const PhoneCardList: React.FC<cardListInterface> = ({ phoneList }) => {
             </div>
             <div className="pagination">
                 {
-                    structureList.pages.map((page: number, index: number, pages: any) => {
-                        if (page === 1 || page === pages.length) {
+                    paginationList.map((page: number, index: number, list: any) => {
+                        const elem = (
+                            <div
+                                className={`pagination__item${page === structureList.currentPage ? ' selected' : ''}`}
+                                onClick={(event: any) => updatePagination(page)}
+                            >
+                                {page}
+                            </div>
+                        )
+                        if (index === 0 && page + 1 !== list[index + 1]) {
                             return (
-                                <div className={`pagination__item ${page === structureList.currentPage ? ' selected' : ''}`} onClick={(event: any) => {
-                                    const { data, onPage } = structureList;
-
-
-
-                                    setStructureList({
-                                        ...structureList,
-                                        currentPage: page,
-                                        currentVissible: data.slice((page - 1) * onPage, onPage * page + 1)
-                                    })
-                                }}>{page}</div>
+                                <>
+                                    {elem}
+                                    <div>...</div>
+                                </>
                             )
                         }
 
+                        if (index === list.length - 1) {
+                            console.log(index,list[list.length - 2] , page);
 
-
-
-
-
-                        // if (  structureList.currentPage !== page && ) {
-                        //     return (
-                        //         <div className={`pagination__item ${page === structureList.currentPage ? ' selected': ''}`} onClick={(event:any) => {
-                        //             const {data, onPage} = structureList;
-
-                        //             setStructureList({
-                        //                 ...structureList,
-                        //                 currentPage: page,
-                        //                 currentVissible: data.slice((page-1)*onPage, onPage*page + 1)
-                        //             })
-                        //         }}>{page}</div>   
-                        //     )
-                        // }
-
-
-                        if (structureList.currentPage <= 5 && page <= 5) {
-                            return (
-                                <div className={`pagination__item ${page === structureList.currentPage ? ' selected' : ''}`} onClick={(event: any) => {
-                                    const { data, onPage } = structureList;
-
-                                    setStructureList({
-                                        ...structureList,
-                                        currentPage: page,
-                                        currentVissible: data.slice((page - 1) * onPage, onPage * page + 1)
-                                    })
-                                }}>{page}</div>
-                            )
                         }
 
-                        if (page === structureList.currentPage || page - 1 === structureList.currentPage || page + 1 == structureList.currentPage
-                            || page - 2 === structureList.currentPage || page + 2 == structureList.currentPage
-                            // || page - 3 === structureList.currentPage || page + 3 == structureList.currentPage
-                        ) {
-                            return (
-                                <div className={`pagination__item ${page === structureList.currentPage ? ' selected' : ''}`} onClick={(event: any) => {
-                                    const { data, onPage } = structureList;
+                        
 
-                                    setStructureList({
-                                        ...structureList,
-                                        currentPage: page,
-                                        currentVissible: data.slice((page - 1) * onPage, onPage * page + 1)
-                                    })
-                                }}>{page}</div>
+                        if (index === list.length - 1 && list[list.length - 2] !== page - 1 ) {
+                            return (
+                                <>
+                                    <div>...</div>
+                                    {elem}
+                                </>
                             )
                         }
-
+                        return elem
                     })
+
+                    // structureList.pages.map((page: number, index: number, pages: any) => {
+                    //     if (page === 1 || page === pages.length) {
+                    //         return (
+                    //             <div className={`pagination__item ${page === structureList.currentPage ? ' selected' : ''}`} onClick={(event: any) => {
+                    //                 const { data, onPage } = structureList;
+
+
+
+                    //                 setStructureList({
+                    //                     ...structureList,
+                    //                     currentPage: page,
+                    //                     currentVissible: data.slice((page - 1) * onPage, onPage * page + 1)
+                    //                 })
+                    //             }}>{page}</div>
+                    //         )
+                    //     }
+
+
+
+
+
+
+                    //     // if (  structureList.currentPage !== page && ) {
+                    //     //     return (
+                    //     //         <div className={`pagination__item ${page === structureList.currentPage ? ' selected': ''}`} onClick={(event:any) => {
+                    //     //             const {data, onPage} = structureList;
+
+                    //     //             setStructureList({
+                    //     //                 ...structureList,
+                    //     //                 currentPage: page,
+                    //     //                 currentVissible: data.slice((page-1)*onPage, onPage*page + 1)
+                    //     //             })
+                    //     //         }}>{page}</div>   
+                    //     //     )
+                    //     // }
+
+
+                    //     if (structureList.currentPage <= 5 && page <= 5) {
+                    //         return (
+                    //             <div className={`pagination__item ${page === structureList.currentPage ? ' selected' : ''}`} onClick={(event: any) => {
+                    //                 const { data, onPage } = structureList;
+
+                    //                 setStructureList({
+                    //                     ...structureList,
+                    //                     currentPage: page,
+                    //                     currentVissible: data.slice((page - 1) * onPage, onPage * page + 1)
+                    //                 })
+                    //             }}>{page}</div>
+                    //         )
+                    //     }
+
+                    //     if (page === structureList.currentPage || page - 1 === structureList.currentPage || page + 1 == structureList.currentPage
+                    //         || page - 2 === structureList.currentPage || page + 2 == structureList.currentPage
+                    //         // || page - 3 === structureList.currentPage || page + 3 == structureList.currentPage
+                    //     ) {
+                    //         return (
+                    //             <div className={`pagination__item ${page === structureList.currentPage ? ' selected' : ''}`} onClick={(event: any) => {
+                    //                 const { data, onPage } = structureList;
+
+                    //                 setStructureList({
+                    //                     ...structureList,
+                    //                     currentPage: page,
+                    //                     currentVissible: data.slice((page - 1) * onPage, onPage * page + 1)
+                    //                 })
+                    //             }}>{page}</div>
+                    //         )
+                    //     }
+
+                    // })
                 }
             </div>
             {
