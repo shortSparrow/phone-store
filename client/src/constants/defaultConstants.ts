e7b935c4d9187ca1ae970bb024a7efd6fb9eca69
Senya <formobileprima@gmail.com>
2020-11-28 19:21:15 +0200

    fef

--- client/src/constants/defaultConstants.ts
+++ client/src/constants/defaultConstants.ts
@@ -1,4 +1,5 @@
 export const defaultConstatnts = {
-    domain: "http://localhost:5000",
+    // domain: "http://localhost:5000",
+    domain: "https://phone-store-test.herokuapp.com/",
     clientHost: "http://localhost:3000"
 }
