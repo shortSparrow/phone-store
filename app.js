71eb5c553a0568b05e557bf743a067f9b76aa125
Senya <formobileprima@gmail.com>
2020-11-28 18:26:58 +0200

    fefe

--- app.js
+++ app.js
@@ -1,46 +1,20 @@
-const express = require("express");
-const config = require('config');
-const mongoose = require("mongoose");
-cors = require('cors')
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const path = require('path')
 const app = express();
 
-app.use(cors())
-app.use(express.json({ extented: true })); // что бы body было не undefined, а объекьтом с полями {}
-
-app.use("/public", express.static("public"));
-
-app.use('/api/auth', require('./routes/auth.routes'))
-
-app.use('/api/phone', require('./routes/phone.routes'))
-app.use('/api/tablet', require('./routes/tablets.routes'))
-
-
+// require('./database');
+app.use(bodyParser.json());
+app.use(cors());
+// API
+// const users = require('/api/users');
+// app.use('/api/users', users);
 app.use(express.static(path.join(__dirname, '../build')))
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../build'))
 })
-
-
-
-
-const PORT = process.env.PORT || config.get('port') || 5000
-
-// const PORT = config.get('port') || 5000
-
-async function start() {
-    try {
-        await mongoose.connect(config.get('mongoUri'), {
-            useNewUrlParser: true,
-            useCreateIndex: true,
-            useUnifiedTopology: true,
-        });
-
-        app.listen(PORT, () => console.log(`Listren server on ${PORT}`))
-    } catch (err) {
-        console.log("Server Error", err.message);
-        process.exit()
-    }
-}
-
-start();
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
