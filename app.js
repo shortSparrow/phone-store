c05021877bb7aaae889803ca3bec9c710d420238
Senya <formobileprima@gmail.com>
2020-11-28 18:17:53 +0200

    fefe

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ cors = require('cors')
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 app.use(cors())
 app.use(express.json({ extented: true })); // что бы body было не undefined, а объекьтом с полями {}
 
