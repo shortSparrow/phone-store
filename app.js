df4a2dbd7fa835536df4a95581a0338a59d51829
Senya <formobileprima@gmail.com>
2020-11-28 18:10:56 +0200

    fefe

--- app.js
+++ app.js
@@ -17,6 +17,17 @@ app.use('/api/tablet', require('./routes/tablets.routes'))
 app.use('/api/test', (req,res) => res.send({Sena: 'dede'}))
 
 
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('client/build'));
+  
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
+
 
 
 const PORT = process.env.PORT || config.get('port') || 5000
