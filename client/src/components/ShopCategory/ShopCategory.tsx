4e83d9a11a869440732509fb8180db0caad70890
Senya <formobileprima@gmail.com>
2020-12-02 22:05:35 +0200

    add route for countDevice count

--- client/src/components/ShopCategory/ShopCategory.tsx
+++ client/src/components/ShopCategory/ShopCategory.tsx
@@ -1,50 +1,71 @@
-import React, { FC } from 'react';
-import { connect } from 'react-redux';
+import React, { FC } from "react";
+import { connect } from "react-redux";
 import { useHistory } from "react-router-dom";
-import { DeviceCountType } from '../../interfaces/appStateInterface';
-import { RootStateInterface } from '../../interfaces/rootStateInterface';
+import { DeviceCountType } from "../../interfaces/appStateInterface";
+import { RootStateInterface } from "../../interfaces/rootStateInterface";
 
 import "./ShopCategory.scss";
 
 const ShopCategory: FC<ShopCategoryInterface> = ({ deviceCount }) => {
-    const history = useHistory()
+  const history = useHistory();
 
-    return (
-        <div className="shop-category__list">
-            <div className="shop-category__item">
-                <img src="./icons/phones-catalog-preview.jpg" alt="" className="shop-category__image" onClick={() => history.push('/phones')} />
-                <div className="shop-category__description">
-                    <p className="shop-category__title">Mobile phones</p>
-                    <p className="small-text shop-category__amount">{deviceCount.phones} models</p>
-                </div>
-            </div>
-
-            <div className="shop-category__item">
-                <img src="./icons/tablets-category-preview.png" alt="" className="shop-category__image" onClick={() => history.push('/tablets')} />
-                <div className="shop-category__description">
-                    <p className="shop-category__title">Tablets</p>
-                    <p className="small-text shop-category__amount">{deviceCount.tablets} models</p>
-                </div>
-            </div>
-
-            <div className="shop-category__item">
-                <img src="./icons/accsessories-catalog-preview.jpg" alt="" className="shop-category__image" onClick={() => history.push('/accessories')} />
-                <div className="shop-category__description">
-                    <p className="shop-category__title">Accessories</p>
-                    <p className="small-text shop-category__amount"> {deviceCount.accessories} models</p>
-                </div>
-            </div>
+  return (
+    <div className="shop-category__list">
+      <div className="shop-category__item">
+        <img
+          src="./icons/phones-catalog-preview.jpg"
+          alt=""
+          className="shop-category__image"
+          onClick={() => history.push("/phones")}
+        />
+        <div className="shop-category__description">
+          <p className="shop-category__title">Mobile phones</p>
+          <p className="small-text shop-category__amount">
+            {deviceCount.phones} models
+          </p>
         </div>
-    )
-}
+      </div>
+
+      <div className="shop-category__item">
+        <img
+          src="./icons/tablets-category-preview.png"
+          alt=""
+          className="shop-category__image"
+          onClick={() => history.push("/tablets")}
+        />
+        <div className="shop-category__description">
+          <p className="shop-category__title">Tablets</p>
+          <p className="small-text shop-category__amount">
+            {deviceCount.tablets} models
+          </p>
+        </div>
+      </div>
+
+      <div className="shop-category__item">
+        <img
+          src="./icons/accsessories-catalog-preview.jpg"
+          alt=""
+          className="shop-category__image"
+          onClick={() => history.push("/accessories")}
+        />
+        <div className="shop-category__description">
+          <p className="shop-category__title">Accessories</p>
+          <p className="small-text shop-category__amount">
+            {" "}
+            {deviceCount.accessories} models
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 interface ShopCategoryInterface {
-    deviceCount: DeviceCountType
+  deviceCount: DeviceCountType;
 }
 
 const mapStateToProps = (state: RootStateInterface, ownProps: any) => ({
-    deviceCount: state.appState.deviceCount
-})
+  deviceCount: state.appState.deviceCount,
+});
 
-
-export default connect(mapStateToProps, null)(ShopCategory)
+export default connect(mapStateToProps, null)(ShopCategory);
--- client/src/pages/PhonesPage/PhonesPage.tsx
+++ client/src/pages/PhonesPage/PhonesPage.tsx
@@ -1,282 +1,301 @@
-import React, { ChangeEvent, useEffect, useState } from 'react';
-import Header from '../../components/Header/Header';
+import React, { ChangeEvent, useEffect, useState } from "react";
+import Header from "../../components/Header/Header";
 
-import './PhonesPage.scss';
-import { connect } from 'react-redux';
-import { RootStateInterface } from '../../interfaces/rootStateInterface';
-import { phones } from '../../store/actions'
-import { phoneCardInterface, phoneListStateType } from '../../interfaces/phonesInterfaces';
-import { DeviceCountType, DeviceScreenType } from '../../interfaces/appStateInterface';
-import DeviceCardList from '../../components/DeviceCardList/DeviceCardList';
-import SmallNavigation from '../../components/SmallNavigation/SmallNavigation';
-import { Select } from '../../components/Select/Select';
-import { Preloader } from '../../components/Preloader/Preloader';
-import { Pagination } from '../../components/Pagination/Pagination';
-import { phone } from '../../store/reducers/phones';
-import Footer from '../../components/Footer/Footer';
+import "./PhonesPage.scss";
+import { connect } from "react-redux";
+import { RootStateInterface } from "../../interfaces/rootStateInterface";
+import { phones } from "../../store/actions";
+import {
+  phoneCardInterface,
+  phoneListStateType,
+} from "../../interfaces/phonesInterfaces";
+import {
+  DeviceCountType,
+  DeviceScreenType,
+} from "../../interfaces/appStateInterface";
+import DeviceCardList from "../../components/DeviceCardList/DeviceCardList";
+import SmallNavigation from "../../components/SmallNavigation/SmallNavigation";
+import { Select } from "../../components/Select/Select";
+import { Preloader } from "../../components/Preloader/Preloader";
+import { Pagination } from "../../components/Pagination/Pagination";
+import { phone } from "../../store/reducers/phones";
+import Footer from "../../components/Footer/Footer";
 
 const selectList = [
-    {
-        title: 'Rich',
-        value: 'rich'
-    },
-    {
-        title: 'Cheap',
-        value: 'cheap'
-    }
-]
+  {
+    title: "Rich",
+    value: "rich",
+  },
+  {
+    title: "Cheap",
+    value: "cheap",
+  },
+];
 
 const selectitemsOnPageList = [
-    {
-        title: '5',
-        value: 5
-    },
-    {
-        title: '10',
-        value: 10
-    },
-    {
-        title: '15',
-        value: 15
-    },
-    {
-        title: '25',
-        value: 25
-    },
-    {
-        title: '50',
-        value: 50
-    },
-]
+  {
+    title: "5",
+    value: 5,
+  },
+  {
+    title: "10",
+    value: 10,
+  },
+  {
+    title: "15",
+    value: 15,
+  },
+  {
+    title: "25",
+    value: 25,
+  },
+  {
+    title: "50",
+    value: 50,
+  },
+];
 
 const PhonesPage: React.FC<mainPropsInterfaces> = (props) => {
-    const {
-        phoneList, loadPhones,
-        setPhoneListState, phoneListState,
-        phoneLoadSuccss, deviceCount
-    } = props
-    const [searchField, setSearchField] = useState('');
-    const [selectedSortValue, setSelectedSortValue] = useState({
-        title: 'Cheap',
-        value: 'cheap'
-    });
+  const {
+    phoneList,
+    loadPhones,
+    setPhoneListState,
+    phoneListState,
+    phoneLoadSuccss,
+    deviceCount,
+  } = props;
+  const [searchField, setSearchField] = useState("");
+  const [selectedSortValue, setSelectedSortValue] = useState({
+    title: "Cheap",
+    value: "cheap",
+  });
 
-    const [selectedItemsOnPAgeValue, setSelectedItemsOnPageValue] = useState({
-        title: '5',
-        value: 5
-    });
+  const [selectedItemsOnPAgeValue, setSelectedItemsOnPageValue] = useState({
+    title: "5",
+    value: 5,
+  });
 
-    const [structureList, setStructureList] = useState<any>({
-        onPage: 16,
+  const [structureList, setStructureList] = useState<any>({
+    onPage: 16,
+    currentPage: 1,
+    pages: [],
+    data: [],
+    currentVissible: [],
+    loaded: false,
+  });
+
+  useEffect(() => {
+    if (phoneList.length) {
+      setStructureList({
+        ...structureList,
+        // onPage: 5,
+        // currentPage: 1,
+        // pages: Array.from(Array(Math.ceil(phoneList.length / 5)).keys(), (_, i) => i + 1),
+        // data: phoneList,
+        // currentVissible: phoneList.slice(0, 5),
+        loaded: true,
+      });
+    }
+  }, [phoneList]);
+
+  // console.log(structureList.currentVissible);
+
+  const handlePageStructure = (value: number) => {
+    const { onPage, data } = structureList;
+
+    setStructureList({
+      ...structureList,
+      onPage: value,
+      pages: Array.from(
+        Array(Math.ceil(phoneListState.visible.length / value)),
+        (_, i) => i + 1
+      ),
+      currentVissible: phoneListState.visible.slice(0, value),
+      currentPage: 1,
+      data: phoneListState.visible,
+    });
+  };
+
+  useEffect(() => {
+    if (phoneList.length) {
+      // console.log(structureList);
+      // console.log('phoneListState: ', phoneListState.visible);
+      // console.log('curentVisible: ', [...phoneListState.visible].slice(0, structureList.onPage));
+
+      setStructureList({
+        ...structureList,
+        data: phoneListState.visible,
+        pages: Array.from(
+          Array(
+            Math.ceil(phoneListState.visible.length / structureList.onPage)
+          ),
+          (_, i) => i + 1
+        ),
+        currentVissible: [...phoneListState.visible].slice(
+          0,
+          structureList.onPage
+        ),
         currentPage: 1,
-        pages: [],
-        data: [],
-        currentVissible: [],
-        loaded: false
-    })
-
-    useEffect(() => {
-        if (phoneList.length) {
-
-            setStructureList({
-                ...structureList,
-                // onPage: 5,
-                // currentPage: 1,
-                // pages: Array.from(Array(Math.ceil(phoneList.length / 5)).keys(), (_, i) => i + 1),
-                // data: phoneList,
-                // currentVissible: phoneList.slice(0, 5),
-                loaded: true
-            })
-        }
-    }, [phoneList])
-
-    // console.log(structureList.currentVissible);
-
-
-    const handlePageStructure = (value: number) => {
-        const { onPage, data } = structureList;
-
-        setStructureList({
-            ...structureList,
-            onPage: value,
-            pages: Array.from(Array(Math.ceil(phoneListState.visible.length / value)), (_, i) => i + 1),
-            currentVissible: phoneListState.visible.slice(0, value),
-            currentPage: 1,
-            data: phoneListState.visible
-        })
+      });
     }
+  }, [phoneListState]);
 
-    useEffect(() => {
+  useEffect(() => {
+    loadPhones();
+  }, []);
 
-        if (phoneList.length) {
-            // console.log(structureList);
-            // console.log('phoneListState: ', phoneListState.visible);
-            // console.log('curentVisible: ', [...phoneListState.visible].slice(0, structureList.onPage));
+  useEffect(() => {
+    handleSort(selectedSortValue.value);
+  }, [selectedSortValue]);
 
+  const handleVisible = (event: ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    setSearchField(value);
 
+    const filtered = [...phoneListState.sorted].filter((phone) =>
+      phone.title.toLocaleLowerCase().includes(value.toLocaleLowerCase())
+    );
 
-            setStructureList({
-                ...structureList,
-                data: phoneListState.visible,
-                pages: Array.from(Array(Math.ceil(phoneListState.visible.length / structureList.onPage)), (_, i) => i + 1),
-                currentVissible: [...phoneListState.visible].slice(0, structureList.onPage),
-                currentPage: 1,
-            })
-        }
+    // console.log('filtered: ', filtered);
 
-    }, [phoneListState])
+    setPhoneListState({
+      ...phoneListState,
+      visible: filtered,
+    });
+  };
 
-    useEffect(() => {
-        loadPhones();
-    }, []);
+  const handleSort = (value: string) => {
+    // sort all items
+    const sortedList = [...phoneList].sort((a, b) => {
+      if (value === "rich") {
+        return +b.price.current.slice(1) - +a.price.current.slice(1);
+      } else if (value === "cheap") {
+        return +a.price.current.slice(1) - +b.price.current.slice(1);
+      } else {
+        return 1;
+      }
+    });
 
-    useEffect(() => {
-        handleSort(selectedSortValue.value);
-    }, [selectedSortValue])
+    // sort visible part
+    const sortedVisible = [...phoneListState.visible].sort((a, b) => {
+      if (value === "rich") {
+        return +b.price.current.slice(1) - +a.price.current.slice(1);
+      } else if (value === "cheap") {
+        return +a.price.current.slice(1) - +b.price.current.slice(1);
+      } else {
+        return 1;
+      }
+    });
 
-    const handleVisible = (event: ChangeEvent<HTMLInputElement>) => {
-        const { value } = event.target
-        setSearchField(value)
+    setPhoneListState({
+      ...phoneListState,
+      sorted: sortedList,
+      visible: sortedVisible,
+      currentSortedValue: value,
+    });
+  };
 
-        const filtered = [...phoneListState.sorted].filter(phone => phone.title.toLocaleLowerCase().includes(value.toLocaleLowerCase()));
+  useEffect(() => {
+    handlePageStructure(selectedItemsOnPAgeValue.value);
+    // console.log(selectedItemsOnPAgeValue);
+  }, [selectedItemsOnPAgeValue]);
 
-        // console.log('filtered: ', filtered);
+  return (
+    <div className="phones-page page">
+      <Header>
+        <input
+          id="filter-field"
+          type="text"
+          value={searchField}
+          onChange={handleVisible}
+          className="filter-input--wrapper filter-input--search"
+          placeholder="Search in phones..."
+        />
+      </Header>
 
-        setPhoneListState({
-            ...phoneListState,
-            visible: filtered,
-        })
-    }
-
-    const handleSort = (value: string) => {
-        // sort all items
-        const sortedList = [...phoneList].sort((a, b) => {
-            if (value === 'rich') {
-                return +b.price.current.slice(1,) - +a.price.current.slice(1,)
-            } else if (value === 'cheap') {
-                return +a.price.current.slice(1,) - +b.price.current.slice(1,)
-            } else {
-                return 1
-            }
-        });
-
-        // sort visible part
-        const sortedVisible = [...phoneListState.visible].sort((a, b) => {
-            if (value === 'rich') {
-                return +b.price.current.slice(1,) - +a.price.current.slice(1,)
-            } else if (value === 'cheap') {
-                return +a.price.current.slice(1,) - +b.price.current.slice(1,)
-            } else {
-                return 1
-            }
-        });
-
-        setPhoneListState({
-            ...phoneListState,
-            sorted: sortedList,
-            visible: sortedVisible,
-            currentSortedValue: value
-        })
-    }
-
-
-    useEffect(() => {
-        handlePageStructure(selectedItemsOnPAgeValue.value)
-        // console.log(selectedItemsOnPAgeValue);
-
-    }, [selectedItemsOnPAgeValue])
-
-    return (
-        <div className="phones-page page">
-            <Header>
-                <input
-                    id="filter-field"
-                    type="text"
-                    value={searchField}
-                    onChange={handleVisible}
-                    className="filter-input--wrapper filter-input--search"
-                    placeholder="Search in phones..."
+      <div className="main-limit">
+        <SmallNavigation params={[{ title: "Phones", link: "/phones" }]} />
+        <p className="main-title page-name-title">Mobile phones</p>
+        <p className="small-text models-count">
+          {structureList.loaded && structureList?.data?.length
+            ? structureList.data.length
+            : deviceCount.phones ?? (
+                <Preloader
+                  color="#89939A"
+                  width="12"
+                  height="12"
+                  borderWidth="1"
+                  wrapperWidth="12"
+                  wrapperHeight="12"
                 />
-            </Header>
+              )}{" "}
+          models
+        </p>
 
-            <div className="main-limit">
-                <SmallNavigation params={[{ title: 'Phones', link: '/phones' }]} />
-                <p className="main-title page-name-title">Mobile phones</p>
-                <p className="small-text models-count">{
-                    structureList.loaded && structureList?.data?.length
-                        ? structureList.data.length
-                        : deviceCount.phones ?? <Preloader
-                            color="#89939A"
-                            width="12"
-                            height="12"
-                            borderWidth="1"
-                            wrapperWidth="12"
-                            wrapperHeight="12"
-                        />
-                } models</p>
+        <div className="phones-page__select-phones--wrapper">
+          <div>
+            <p className="small-text models-count"> Sort by</p>
+            <Select
+              itemList={selectList}
+              setSelectedItem={setSelectedSortValue}
+              selectedItem={selectedSortValue}
+            />
+          </div>
 
-
-
-                <div className="phones-page__select-phones--wrapper">
-                    <div>
-                        <p className="small-text models-count"> Sort by</p>
-                        <Select itemList={selectList} setSelectedItem={setSelectedSortValue} selectedItem={selectedSortValue} />
-                    </div>
-
-                    <div>
-                        <p className="small-text models-count"> Items on page</p>
-                        <Select
-                            itemList={selectitemsOnPageList}
-                            setSelectedItem={setSelectedItemsOnPageValue}
-                            selectedItem={selectedItemsOnPAgeValue}
-                            defaultSelectIndex={selectitemsOnPageList.findIndex((item) => item.value === structureList.onPage)}
-                            width={128}
-                        />
-                    </div>
-                </div>
-
-
-
-                <DeviceCardList deviceList={structureList.currentVissible} />
-
-                <div className="phones-page__pagination-warpper">
-                    <Pagination
-                        structureList={structureList}
-                        setStructureList={setStructureList}
-                        leftSpace={2}
-                        rightSpace={2}
-                    />
-                </div>
-            </div>
-            <Footer />
+          <div>
+            <p className="small-text models-count"> Items on page</p>
+            <Select
+              itemList={selectitemsOnPageList}
+              setSelectedItem={setSelectedItemsOnPageValue}
+              selectedItem={selectedItemsOnPAgeValue}
+              defaultSelectIndex={selectitemsOnPageList.findIndex(
+                (item) => item.value === structureList.onPage
+              )}
+              width={128}
+            />
+          </div>
         </div>
-    )
-}
+
+        <DeviceCardList deviceList={structureList.currentVissible} />
+
+        <div className="phones-page__pagination-warpper">
+          <Pagination
+            structureList={structureList}
+            setStructureList={setStructureList}
+            leftSpace={2}
+            rightSpace={2}
+          />
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
 
 interface mainPropsInterfaces {
-    deviceScreen: DeviceScreenType,
-    phoneList: phoneCardInterface[],
-    phoneListState: any,
-    deviceCount: DeviceCountType,
-    loadPhones: () => {},
-    setPhoneListState: (phoneListState: phoneListStateType) => {},
-    phoneLoadSuccss: (data: phoneCardInterface[]) => {}
+  deviceScreen: DeviceScreenType;
+  phoneList: phoneCardInterface[];
+  phoneListState: any;
+  deviceCount: DeviceCountType;
+  loadPhones: () => {};
+  setPhoneListState: (phoneListState: phoneListStateType) => {};
+  phoneLoadSuccss: (data: phoneCardInterface[]) => {};
 }
 
 const mapStateToProps = (state: RootStateInterface, ownProps: any) => ({
-    deviceScreen: state.appState.deviceScreen,
-    phoneList: state.phone.phoneList,
-    phoneListState: state.phone.phoneListState,
-    phoneState: state.phone.phoneListState,
-    deviceCount: state.appState.deviceCount
-})
+  deviceScreen: state.appState.deviceScreen,
+  phoneList: state.phone.phoneList,
+  phoneListState: state.phone.phoneListState,
+  phoneState: state.phone.phoneListState,
+  deviceCount: state.appState.deviceCount,
+});
 
 const mapDispatchToProps = (dispatch: any) => ({
-    loadPhones: () => dispatch(phones.loadPhones()),
-    setPhoneListState: (phoneListState: phoneListStateType) => dispatch(phones.phoneListState(phoneListState)),
-    phoneLoadSuccss: (data: phoneCardInterface[]) => dispatch(phones.phoneSuccess(data))
-})
+  loadPhones: () => dispatch(phones.loadPhones()),
+  setPhoneListState: (phoneListState: phoneListStateType) =>
+    dispatch(phones.phoneListState(phoneListState)),
+  phoneLoadSuccss: (data: phoneCardInterface[]) =>
+    dispatch(phones.phoneSuccess(data)),
+});
 
-
-
-export default connect(mapStateToProps, mapDispatchToProps)(PhonesPage)
+export default connect(mapStateToProps, mapDispatchToProps)(PhonesPage);
